2b135f734cda5359156de0a2cd5277134bda1f7c
lu-jiayi <jlg363@stanford.edu>
2020-04-28 01:15:59 -0700

    add exp 2

--- analysis/exp1b/clustering_analysis_1b.R
+++ analysis/exp1b/clustering_analysis_1b.R
@@ -7,7 +7,8 @@ library(gtable)
 library(lme4)
 library(tidyverse)
 library(CRPClustering)
-
+library(cluster)
+library(factoextra)
 `%notin%` <- Negate(`%in%`)
 data<-read.csv("satiation_1b_98item-trials.csv")
 
@@ -51,10 +52,12 @@ for (i in (1:length(all_filler$subject))){
 data = subset(data, workerid %in% eligible_subjects)
 
 #Step 3: exclude non-English speakers
-non_Eng <- c(119)
+non_Eng <- c()
 
 data = subset(data, workerid %notin% non_Eng)
 
+
+#clustering analysis (k-means)
 all_vec <- data.frame(workerid = integer(), wh_intercept=double(), wh_slope = double(),
                       subj_intercept = double(), subj_slope = double(), 
                       cnpc_intercept = double(), cnpc_slope = double())
@@ -75,8 +78,8 @@ for (i in data$workerid){
     wh <- fitwh$coefficients
     subj <- fitsubj$coefficients
     cnpc <- fitcnpc$coefficients
-   
-     all_vec[nrow(all_vec) + 1,] = c(i, wh, subj, cnpc)
+    
+    all_vec[nrow(all_vec) + 1,] = c(i, wh, subj, cnpc)
     print(c(i, wh, subj, cnpc))
   }
   
@@ -84,39 +87,139 @@ for (i in data$workerid){
 
 all_vec_features = all_vec
 all_vec_features$workerid <- NULL
-results<-kmeans(all_vec_features, 3)
-all_vec$cluster <- results$cluster
-results
+
+df <- scale(all_vec_features)
+distance <- get_dist(df)
+distance_graph = fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+k2 <- kmeans(df, centers = 2, nstart = 25)
+k3 <- kmeans(df, centers = 3, nstart = 25)
+k4 <- kmeans(df, centers = 4, nstart = 25)
+k5 <- kmeans(df, centers = 5, nstart = 25)
+
+# plots to compare
+p1 <- fviz_cluster(k2, geom = "point", data = df) + ggtitle("k = 2")
+p2 <- fviz_cluster(k3, geom = "point",  data = df) + ggtitle("k = 3")
+p3 <- fviz_cluster(k4, geom = "point",  data = df) + ggtitle("k = 4")
+p4 <- fviz_cluster(k5, geom = "point",  data = df) + ggtitle("k = 5")
+
+library(gridExtra)
+grid.arrange(p1, p2, p3, p4, nrow = 2)
+
+#silhouettes method to determine optimal number of clusters
+sil<- fviz_nbclust(df, kmeans, method = "silhouette")
+#gap-statistics method
+
+set.seed(123)
+gap_stat <- clusGap(df, FUN = kmeans, nstart = 25,
+                    K.max = 10, B = 50)
+## Print the result
+print(gap_stat, method = "firstmax")
+gap <- fviz_gap_stat(gap_stat)
+
+#elbow-method
+set.seed(123)
+
+# function to compute total within-cluster sum of square 
+wss <- function(k) {
+  kmeans(df, k, nstart = 10 )$tot.withinss
+}
+
+# Compute and plot wss for k = 1 to k = 15
+k.values <- 1:15
+
+# extract wss for 2-15 clusters
+wss_values <- map_dbl(k.values, wss)
+
+elbow <- plot(k.values, wss_values,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of clusters K",
+     ylab="Total within-clusters sum of squares")
 
 
- plot(all_vec[c("wh_intercept", "wh_slope")], col = results$cluster)
- plot(all_vec[c("subj_intercept", "subj_slope")], col = results$cluster)
- plot(all_vec[c("wh_intercept", "wh_slope", "cnpc_intercept", "cnpc_slope", "subj_intercept", "subj_slope")])
- 
- 
- ##CRPClustering
- slope_vec <- all_vec_features
- slope_vec$cnpc_intercept <- NULL
- slope_vec$subj_intercept <- NULL
- slope_vec$wh_intercept <- NULL
- 
- intercept_vec <- all_vec_features
- intercept_vec$cnpc_slope <- NULL
- intercept_vec$subj_slope <- NULL
- intercept_vec$wh_slope <- NULL
- 
- 
- 
- z_results<- crp_gibbs(as.matrix(all_vec_features),
-                       mu = c(mean(all_vec_features$wh_intercept),
-                              mean(all_vec_features$wh_slope),
-                              mean(all_vec_features$subj_intercept),
-                              mean(all_vec_features$subj_slope),
-                              mean(all_vec_features$cnpc_intercept),
-                              mean(all_vec_features$cnpc_slope)),
-                       iteration = 2000)
- 
-z_results
+all_vec$cluster <- k2$cluster
+cluster_1 <- subset(all_vec, cluster == 1)
+cluster_2 <- subset(all_vec, cluster == 2)
 
- graph = crp_graph_2d(as.matrix(intercept_vec), z_results)
- 
+data_1 <- subset(data, workerid %in% cluster_1$workerid)
+data_2 <- subset(data, workerid %in% cluster_2$workerid)
+
+c_clean1 = ggplot(data_1, aes(x=trial_sequence_total, y=response, color = condition, shape = condition)) + 
+  
+  geom_smooth(method=lm, aes(fill=condition))+theme_bw()
+
+c_clean2 = ggplot(data_2, aes(x=trial_sequence_total, y=response, color = condition, shape = condition)) + 
+  
+  geom_smooth(method=lm, aes(fill=condition))+theme_bw()
+library(ggpubr)
+theme_set(theme_pubr())
+figure <- ggarrange(c_clean1, c_clean2,
+                    labels = c("Cluster A: n=57", "Cluster B: n=90"),
+                    ncol = 1, nrow = 2)
+ggsave("cluster_1b.pdf", width=10, height = 12)
+
+all_vec$cluster <- k4$cluster
+cluster_1 <- subset(all_vec, cluster == 1)
+cluster_2 <- subset(all_vec, cluster == 2)
+cluster_3 <- subset(all_vec, cluster == 3)
+cluster_4 <- subset(all_vec, cluster == 4)
+
+data_1 <- subset(data, workerid %in% cluster_1$workerid)
+data_2 <- subset(data, workerid %in% cluster_2$workerid)
+data_3 <- subset(data, workerid %in% cluster_3$workerid)
+data_4 <- subset(data, workerid %in% cluster_4$workerid)
+
+c_clean1 = ggplot(data_1, aes(x=trial_sequence_total, y=response, color = condition, shape = condition)) + 
+  
+  geom_smooth(method=lm, aes(fill=condition))+theme_bw()
+
+c_clean2 = ggplot(data_2, aes(x=trial_sequence_total, y=response, color = condition, shape = condition)) + 
+  
+  geom_smooth(method=lm, aes(fill=condition))+theme_bw()
+c_clean3 = ggplot(data_3, aes(x=trial_sequence_total, y=response, color = condition, shape = condition)) + 
+  
+  geom_smooth(method=lm, aes(fill=condition))+theme_bw()
+c_clean4 = ggplot(data_4, aes(x=trial_sequence_total, y=response, color = condition, shape = condition)) + 
+  
+  geom_smooth(method=lm, aes(fill=condition))+theme_bw()
+library(ggpubr)
+theme_set(theme_pubr())
+figure4 <- ggarrange(c_clean1, c_clean2, c_clean3, c_clean4,
+                     labels = c("Cluster A", "Cluster B", "Cluster C", "Cluster D"),
+                     ncol = 2, nrow = 2)
+ggsave("cluster_1b_cluster4.pdf", width=10, height = 24)
+##CRPClustering
+# slope_vec <- all_vec_features
+# slope_vec$cnpc_intercept <- NULL
+# slope_vec$subj_intercept <- NULL
+# slope_vec$wh_intercept <- NULL
+# 
+# intercept_vec <- all_vec_features
+# intercept_vec$cnpc_slope <- NULL
+# intercept_vec$subj_slope <- NULL
+# intercept_vec$wh_slope <- NULL
+# 
+# 
+# 
+# z_results<- crp_gibbs(as.matrix(slope_vec),
+#                       mu = c(0,0,0),
+#                       iteration = 100)
+# 
+# 
+##CRPClustering
+
+# slope_vec <- all_vec_features
+# slope_vec$cnpc_intercept <- NULL
+# slope_vec$subj_intercept <- NULL
+# slope_vec$wh_intercept <- NULL
+# 
+# intercept_vec <- all_vec_features
+# intercept_vec$cnpc_slope <- NULL
+# intercept_vec$subj_slope <- NULL
+# intercept_vec$wh_slope <- NULL
+# 
+
+
+#z_results<- crp_gibbs(as.matrix(slope_vec),
+#                      mu = c(0,0,0),
+#                      iteration = 100)
+
---
+++ experiment/satiation_exp2/README.md
@@ -0,0 +1,12 @@
+
+## Satiation Baseline (replication of Snyder (2000)), treating island items as ungrammatical in terms of balancing ##
+
+We are using a template for web-based experiments developed and used in Noah Goodman's CoCoLab.
+
+### Experiment files
+Experiment files can be found in the experiment folder. All stimuli used can be found in the javascript file. 
+The experiment uses a balanced dataset: 15 blocks in total (3 island items, 1 grammatical filler, and 1 word salad item per block). 
+
+
+### Analysis files
+R codes used for data processing and analysis can be found in the analysis folder. 
