00bcd767995394d520daf60fcf942e2d229f9cab
thegricean <judithdegen@gmail.com>
2020-05-07 18:28:38 -0700

    cleaned up analysis directory and reformulated test means plot in tidyverse

--- analysis/exp2/rscripts/data_processing_2.R
+++ analysis/exp2/rscripts/data_processing_2.R
@@ -1,17 +1,21 @@
-setwd("C:/Users/aldos/submiterator")
-library(plyr)
-library(dplyr)
-library(reshape)
-library(ggplot2)
+#setwd("C:/Users/aldos/submiterator")
+# set working directory to directory of script
+this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(this.dir)
+
+source('../../helpers.R')
+
 library(gtable)
 library(lme4)
 library(tidyverse)
 library(simr)
 library(brms)
 `%notin%` <- Negate(`%in%`)
-data<-read.csv("satiation_exp2-trials.csv")
+data<-read.csv("../../../experiment/satiation_exp2/results/satiation_exp2-trials.csv")
 test_cond = "CNPC"
 
+# color-blind-friendly colors:
+cbPalette = c("#d55e00", "#009e74","#e69d00","#f0e442","cc79a7", "#0071b2")
 
 #Step 1: Filter out the participants who responded incorrectely more than once to the practice questions:
 practice_data=subset(data,block_sequence == "practice")
@@ -23,12 +27,6 @@ data=subset(data, is.element(workerid, practice_good_data$workerid))
 
 filler_data = subset(data, condition == "FILL")
 ungram_data = subset(data, condition == "UNGRAM")
-library(bootstrap)
-theta <- function(x,xdata,na.rm=T) {mean(xdata[x],na.rm=na.rm)}
-ci.low <- function(x,na.rm=T) {
-  quantile(bootstrap(1:length(x),1000,theta,x,na.rm=na.rm)$thetastar,.025,na.rm=na.rm)}
-ci.high <- function(x,na.rm=T) {
-  quantile(bootstrap(1:length(x),1000,theta,x,na.rm=na.rm)$thetastar,.975,na.rm=na.rm)}
 
 filler_by_subject = aggregate(filler_data[,"response"],list(filler_data$workerid), mean)
 ungram_by_subject = aggregate(ungram_data[,"response"],list(ungram_data$workerid), ci.high)
@@ -110,66 +108,88 @@ test_data <- subset(d, phase == "test")
 #summary(model_test)
 
 #step8: plot
-summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
-                      conf.interval=.95, .drop=TRUE) {
-  library(plyr)
-  
-  # New version of length which can handle NA's: if na.rm==T, don't count them
-  length2 <- function (x, na.rm=FALSE) {
-    if (na.rm) sum(!is.na(x))
-    else       length(x)
-  }
-  
-  # This does the summary. For each group's data frame, return a vector with
-  # N, mean, and sd
-  datac <- ddply(data, groupvars, .drop=.drop,
-                 .fun = function(xx, col) {
-                   c(N    = length2(xx[[col]], na.rm=na.rm),
-                     mean = mean   (xx[[col]], na.rm=na.rm),
-                     sd   = sd     (xx[[col]], na.rm=na.rm)
-                   )
-                 },
-                 measurevar
-  )
-  
-  # Rename the "mean" column    
-  datac <- rename(datac, c("mean" = measurevar))
-  
-  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
-  
-  # Confidence interval multiplier for standard error
-  # Calculate t-statistic for confidence interval: 
-  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
-  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
-  datac$ci <- datac$se * ciMult
-  
-  return(datac)
-}
-df2 <- summarySE(test_data, measurevar="response", 
-                 groupvars=c("condition", "test_match_cond"))
-head(df2)
+# summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+#                       conf.interval=.95, .drop=TRUE) {
+#   library(plyr)
+#   
+#   # New version of length which can handle NA's: if na.rm==T, don't count them
+#   length2 <- function (x, na.rm=FALSE) {
+#     if (na.rm) sum(!is.na(x))
+#     else       length(x)
+#   }
+#   
+#   # This does the summary. For each group's data frame, return a vector with
+#   # N, mean, and sd
+#   datac <- ddply(data, groupvars, .drop=.drop,
+#                  .fun = function(xx, col) {
+#                    c(N    = length2(xx[[col]], na.rm=na.rm),
+#                      mean = mean   (xx[[col]], na.rm=na.rm),
+#                      sd   = sd     (xx[[col]], na.rm=na.rm)
+#                    )
+#                  },
+#                  measurevar
+#   )
+#   
+#   # Rename the "mean" column    
+#   datac <- rename(datac, c("mean" = measurevar))
+#   
+#   datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+#   
+#   # Confidence interval multiplier for standard error
+#   # Calculate t-statistic for confidence interval: 
+#   # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+#   ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+#   datac$ci <- datac$se * ciMult
+#   
+#   return(datac)
+# }
+# df2 <- summarySE(test_data, measurevar="response", 
+#                  groupvars=c("condition", "test_match_cond"))
+# head(df2)
+# 
+# agg_test <- aggregate(test_data[,"response"], by= list(test_data$workerid, test_data$condition, test_data$test_match_cond), FUN = mean, na.rm=TRUE)
+# 
+# names(agg_test)[names(agg_test) == "Group.1"] <- "workerid"
+# 
+# names(agg_test)[names(agg_test) == "Group.2"] <- "condition"
+# 
+# names(agg_test)[names(agg_test) == "Group.3"] <- "test_match_cond"
+# 
+# names(agg_test)[names(agg_test) == "x"] <- "response"
+# 
+# p<- ggplot(agg_test, aes(x=condition, y=response, fill=test_match_cond)) + 
+#   geom_bar(data = df2, stat="identity", color="black", 
+#            position=position_dodge()) +
+#   geom_errorbar(data = df2, aes(ymin=response-se, ymax=response+se), width=.2,
+#              position=position_dodge(.9)) + theme_bw() + geom_jitter(aes(x=condition, y=response, color=test_match_cond), alpha = .4)
+# 
+# p
 
-agg_test <- aggregate(test_data[,"response"], by= list(test_data$workerid, test_data$condition, test_data$test_match_cond), FUN = mean, na.rm=TRUE)
+test_data = test_data %>%
+  mutate(sentence_type = fct_recode(condition,"grammatical"="FILL","ungrammatical"="UNGRAM")) %>%
+  mutate(sentence_type = fct_relevel(sentence_type,"grammatical","WH","SUBJ","CNPC"))
 
-names(agg_test)[names(agg_test) == "Group.1"] <- "workerid"
+means = test_data %>%
+  group_by(test_match_cond,sentence_type) %>%
+  summarize(Mean = mean(response), CILow = ci.low(response), CIHigh = ci.high(response)) %>%
+  ungroup() %>%
+  mutate(YMin=Mean-CILow,YMax=Mean+CIHigh)
 
-names(agg_test)[names(agg_test) == "Group.2"] <- "condition"
+subj_means = test_data %>%
+  group_by(test_match_cond,sentence_type,workerid) %>%
+  summarize(Mean = mean(response)) %>%
+  ungroup() 
 
-names(agg_test)[names(agg_test) == "Group.3"] <- "test_match_cond"
+dodge = position_dodge(.9)
 
-names(agg_test)[names(agg_test) == "x"] <- "response"
-
-p<- ggplot(agg_test, aes(x=condition, y=response, fill=test_match_cond)) + 
-  geom_bar(data = df2, stat="identity", color="black", 
-           position=position_dodge()) +
-  geom_errorbar(data = df2, aes(ymin=response-se, ymax=response+se), width=.2,
-             position=position_dodge(.9)) + theme_bw() + geom_jitter(aes(x=condition, y=response, color=test_match_cond), alpha = .4)
-
-p
-
-
-
-
+ggplot(means, aes(x=sentence_type,y=Mean,fill=test_match_cond)) +
+  geom_bar(stat="identity",position=dodge,color="black") +
+  geom_point(data=subj_means,alpha=.2,position=dodge,color="gray30") +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25,position=dodge) +
+  scale_fill_manual(values=cbPalette,name="Test condition") +
+  xlab("Sentence type") +
+  ylab("Mean acceptability rating")
+ggsave("../graphs/test_means.pdf",width=5.5,height=3)
 
 #overall plot:
 c= ggplot(exposure_data, aes(x=trial_sequence_total, y=response, color = condition, shape = condition)) + 
